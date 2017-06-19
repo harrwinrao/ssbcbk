943fe42001743ef0f0f11ba9dd09d784ed84225b
harrwinrao <harrwinrao@gmail.com>
2017-06-19 14:14:35 +0800

    Basic setup

--- src/main/java/com/ssbcbk/web/controller/MainController.java
+++ src/main/java/com/ssbcbk/web/controller/MainController.java
@@ -1,9 +1,7 @@
 package com.ssbcbk.web.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -14,13 +12,8 @@ import java.util.Map;
 @Controller
 public class MainController {
 
-    // inject via application.properties
-    @Value("${welcome.message:test}")
-    private String message = "Hello World";
-
     @RequestMapping("/")
-    public String index(Map<String, Object> model) {
-        model.put("message", this.message);
-        return "welcome";
+    public String index() {
+        return "index";
     }
 }
